e497aa72e18a3dfe0aea59d431d6c73623801eae
山本隼也 <j.yama.japan@gmail.com>
2020-10-11 17:49:26 +0900

    add init test case

--- InterButtonTests/InterButtonTests.swift
+++ InterButtonTests/InterButtonTests.swift
@@ -19,18 +19,35 @@ class InterButtonTests: XCTestCase {
     }
 
     func testInit() throws {
-        let interButton = InterButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        let interButton = InterButton()
+        XCTAssertNotNil(interButton)
+    }
+    
+    func testInitFrame() throws {
+        let interButton = InterButton(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        XCTAssertNotNil(interButton)
+    }
+    
+    func testInitCode() throws {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWith: data)
+        archiver.finishEncoding()
+        let nsCoder = NSKeyedUnarchiver(forReadingWith: data as Data)
+        let interButton = InterButton(coder: nsCoder)
         XCTAssertNotNil(interButton)
     }
     
     func testSetDuration() throws {
-        let interButton = InterButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        let interButton = InterButton()
         interButton.duration = 0.5
+        XCTAssertEqual(interButton.duration, 0.5)
+        
     }
     
     func testSetMinimumScale() throws {
-        let interButton = InterButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        let interButton = InterButton()
         interButton.minimumScale = 0.1
+        XCTAssertEqual(interButton.minimumScale, 0.1)
     }
 
 }
