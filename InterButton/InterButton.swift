ebcbc8c22463ad3c92116a3445669841e81b5741
山本隼也 <j.yama.japan@gmail.com>
2020-10-24 23:57:26 +0900

    add haptics

--- InterButton/InterButton.swift
+++ InterButton/InterButton.swift
@@ -13,6 +13,8 @@ public final class InterButton: UIButton {
     @IBInspectable public var minimumScale: CGFloat = 0.98
     @IBInspectable public var duration: TimeInterval = 0.2
     
+    private let feedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -24,15 +26,17 @@ public final class InterButton: UIButton {
     }
     
     private func setup() {
+        feedbackGenerator.prepare()
         addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
         addTarget(self, action: #selector(touchUp(_:)), for: .touchUpInside)
         addTarget(self, action: #selector(touchUp(_:)), for: .touchUpOutside)
     }
     
     @objc private func touchDown(_ sender:UIButton) {
-        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut) {
+        feedbackGenerator.impactOccurred()
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: {
             self.transform = CGAffineTransform(scaleX: self.minimumScale, y: self.minimumScale)
-        }
+        })
     }
     
     @objc private func touchUp(_ sender:UIButton) {
